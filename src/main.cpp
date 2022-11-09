75a1a305867e8b2dbab09b713c80b2ee0905f5df
huxiaofeng3 <yfhuxiaofeng@jd.com>
2022-11-09 19:36:52 +0800

    按钮点击管用了

--- src/main.cpp
+++ src/main.cpp
@@ -22,11 +22,13 @@ private:
     void OnHello(wxCommandEvent& event);
     void OnExit(wxCommandEvent& event);
     void OnAbout(wxCommandEvent& event);
+    wxButton *HelloWorld;
 };
  
 enum
 {
-    ID_Hello = 1
+    ID_Hello = 1,
+    BUTTON_Hello = 2
 };
  
 wxIMPLEMENT_APP(MyApp);
@@ -41,27 +43,16 @@ bool MyApp::OnInit()
 MyFrame::MyFrame()
     : wxFrame(nullptr, wxID_ANY, "Hello World")
 {
-    wxMenu *menuFile = new wxMenu;
-    menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
-                     "Help string shown in status bar for this menu item");
-    menuFile->AppendSeparator();
-    menuFile->Append(wxID_EXIT);
- 
-    wxMenu *menuHelp = new wxMenu;
-    menuHelp->Append(wxID_ABOUT);
- 
-    wxMenuBar *menuBar = new wxMenuBar;
-    menuBar->Append(menuFile, "&File");
-    menuBar->Append(menuHelp, "&Help");
- 
-    SetMenuBar( menuBar );
  
     CreateStatusBar();
     SetStatusText("Welcome to wxWidgets!");
+    HelloWorld = new wxButton(this, BUTTON_Hello, _T("Hello"),
+      // shows a button on this window
+    wxPoint(0,0), wxSize(30,30), 0); // with the text "hello World"
+    // 按钮点击管用了
+    Connect(BUTTON_Hello, wxEVT_COMMAND_BUTTON_CLICKED, 
+      wxCommandEventHandler( MyFrame::OnAbout));
  
-    Bind(wxEVT_MENU, &MyFrame::OnHello, this, ID_Hello);
-    Bind(wxEVT_MENU, &MyFrame::OnAbout, this, wxID_ABOUT);
-    Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
 }
  
 void MyFrame::OnExit(wxCommandEvent& event)
